05e1da53000be092148389b095cc90077d982836
Dinil Valsan <dinil.v@litebreeze.com>
2025-12-31 19:58:01 +0530

    rest

--- src/flashscore_scraper/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Flashscore ML Scraper - Soccer match dataset generator for ML/AI research."""
-
-__version__ = "1.0.0"
--- src/flashscore_scraper/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Utility modules."""
-
-from .parsing import parse_score, parse_date, parse_attendance
-from .retry import with_retry
-
-__all__ = ["parse_score", "parse_date", "parse_attendance", "with_retry"]
